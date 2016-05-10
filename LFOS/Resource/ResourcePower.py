a243120a4d91fb789e779010fe9ee70b8cd78e54
Güner Orhan <gunerorhan2010@gmail.com>
2016-05-10 16:56:15 +0200

    New power consumption module has started to be implemented using Factory Method Design Pattern

--- LFOS/Resource/ResourcePower.py
+++ LFOS/Resource/ResourcePower.py
@@ -6,123 +6,100 @@ class StateGranularity:
     CONT = True
     DISC = False
 
-
-class PowerState:
+class AbstractPowerConsumption:
     def __init__(self, scale, pow_cons):
-        self.scale = scale
-        self.pow_cons = pow_cons
+        self._active_power_state = (scale, pow_cons)
+        self._min_state = (scale, pow_cons)
+        self._max_state = (scale, pow_cons)
 
-    def __eq__(self, other):
-        return (type(other) is PowerState and self.scale == other.scale) or (other == self.scale)
-
-    def __repr__(self):
-        return LOG(msg='Scale: %.2f, PowerConsumption: %.2f' % (self.scale, self.pow_cons), n_print=None)
-
-
-class ResourcePower(list):
-
-    def __init__(self, **kwargs):
-        max_state = kwargs['max_state'] if 'max_state' in kwargs else None
-        min_state = kwargs['min_state'] if 'min_state' in kwargs else None
-
-        if max_state is None and min_state is None:
-            LOG(msg='Max and Min states are not defined', log=Logs.ERROR)
-            exit(-1)
-        elif max_state is None:
-            max_state = min_state
-        elif min_state is None:
-            min_state = max_state
-
-        super(ResourcePower, self).__init__([])
-        self.__max_state = self.__min_state = None
-        self.__n_states = 0
-        self.__granularity = kwargs['granularity']
-        self.__active_state = None
-
-        self.__power_derivative = 0.0
-        self.__power_offset = 0.0
-
-        if kwargs['scalable']:
-            self.__scalable = True
-            self.__max_state = PowerState(max_state[0], max_state[1])
-            self.__min_state = PowerState(min_state[0], min_state[1])
-            self.append(copy(self.__max_state))
-            self.append(copy(self.__min_state))
-            self.__active_state = copy(self.__max_state)
-            self.__n_states = len(self)
-
-            self.__power_derivative = float(self.__max_state.pow_cons - self.__min_state.pow_cons) / (self.__max_state.scale - self.__min_state.scale)
-            self.__power_offset = self.__min_state.pow_cons - (self.__power_derivative * self.__min_state.scale)
-        else:
-            self.__scalable = False
-            self.__max_state = self.__min_state = PowerState(max_state[0], max_state[1])
-            self.append(copy(self.__max_state))
-            self.__active_state = copy(self.__max_state)
-            self.__n_states = len(self)
-
-    # public functions
-    def append(self, scale, pow_cons):
-        if self.__scalable and \
-                self.__granularity == StateGranularity.DISC and \
-                self.__range_check(scale) and \
-                not self.__has_scale(scale):
-            self.append(PowerState(scale, pow_cons))
-            self.__n_states = len(self)
-            LOG(msg='The state is added to power consumption: Scale: %.2f, Power: %.2f' % (scale, pow_cons), log=Logs.INFO)
-        else:
-            LOG(msg='Invalid procedure call: %.2f' % scale, log=Logs.ERROR)
-
-    def append(self, p_object):
-        if isinstance(p_object, PowerState):
-            super(ResourcePower, self).append(p_object)
-        else:
-            self.append(p_object, self.__compute_power_consumption(p_object))
-
-    def remove_state(self, scale):
-        if self.__has_scale(scale):
-            index = self.index(scale)
-            return self.pop(index)
-        else:
-            LOG(msg='Scale is not in the list. Scale=%.3f' % scale, log=Logs.ERROR)
-            return
-
-    def set_power_mode(self, scale):
-        if self.__range_check(scale):
-            if self.__granularity == StateGranularity.CONT and self.__range_check(scale, self.__min_state.scale, self.__max_state.scale):
-                self.__active_state = PowerState(scale, self.__get_power_consumption(scale))
-                LOG(msg='Power is set. Scale: %.2f' % scale)
-                return True
-            elif self.__granularity == StateGranularity.DISC and self.__has_scale(scale):
-                self.__active_state = self.__states[self.__states.index(scale)]
-                LOG(msg='Power is set. Scale: %.2f' % scale)
-                return True
-
-        LOG(msg='Invalid scale. Scale: %.3f' % scale, log=Logs.WARN)
-        return False
-
-    def get_power_mode(self):
-        return self.__active_state
+    def get_power_scale(self):
+        return self._active_power_state[0]
 
     def get_power_consumption(self):
-        return self.__active_state.pow_cons
+        return self._active_power_state[1]
 
-    # Internal functions
-    def __has_scale(self, scale):
-        return scale in self
+    def get_power_state(self):
+        return self._active_power_state
 
-    def __range_check(self, scale, range_min=0.0, range_max=1.0):
-        return (scale <= range_max) and (scale > range_min)
+    def get_max_state(self):
+        return self._max_state
 
-    def __compute_power_consumption(self, scale):
-        return self.__power_derivative * scale + self.__power_offset
+    def get_min_state(self):
+        return self._min_state
 
-    def __get_power_consumption(self, scale):
-        consumption = None
-        if self.__scalable and self.__granularity == StateGranularity.CONT:
-            consumption = self.__compute_power_consumption(scale)
-        elif self.__has_scale(scale):
-            consumption = self[scale]
+    def range_check(self, scale):
+        return self._min_state[0] <= scale <= self._max_state[0]
+
+    def get_power_consumption_w_scale(self, scale):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+    def set_power_mode(self, scale):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+    def add_state(self, scale, pow_cons):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+    def remove_state(self, scale):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+    def set_min_state(self, scale, pow_cons):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+    def set_max_state(self, scale, pow_cons):
+        LOG(msg='Invalid procedure call', log=Logs.ERROR)
+
+class FixedPowerConsumption(AbstractPowerConsumption):
+    def __init__(self, scale, pow_cons):
+        AbstractPowerConsumption.__init__(scale, pow_cons)
+
+
+class DiscreteStatePowerConsumption(AbstractPowerConsumption, dict):
+    def __init__(self, min_scale, min_pow_cons, max_scale, max_pow_cons):
+        dict.__init__({})
+        self[min_scale] = min_pow_cons
+        self[max_scale] = max_pow_cons
+
+        AbstractPowerConsumption.__init__(max_scale, max_pow_cons)
+        self.set_min_state(min_scale, min_pow_cons)
+
+    def get_power_consumption_w_scale(self, scale):
+        if scale in self:
+            return self[scale]
         else:
-            LOG(msg='Invalid request. Scale: %.3f' % scale, log=Logs.WARN)
+            LOG(msg='Given scale is not in the list of power states.', log=Logs.WARN)
 
-        return consumption
+    def set_power_mode(self, scale):
+        if scale in self:
+            self._active_power_state = (scale, self[scale])
+
+    def add_state(self, scale, pow_cons):
+        if not scale in self and self.range_check(scale):
+            self[scale] = pow_cons
+        elif scale in self:
+            LOG(msg='Given power scale is already in the list of power states.', log=Logs.WARN)
+        else:
+            LOG(msg='Given power scale is not within min and max power state scope.', log=Logs.WARN)
+
+    def remove_state(self, scale):
+        try:
+            return self.pop(scale)
+        except KeyError:
+            LOG(msg='Given scale is not in the list of power states.')
+            return None
+
+    def set_max_state(self, scale, pow_cons):
+        if scale > self.get_max_state():
+            self._max_state = (scale, pow_cons)
+            self[scale] = pow_cons
+        else:
+            LOG(msg='Current max power state is already higher.', log=Logs.WARN)
+
+    def set_min_state(self, scale, pow_cons):
+        if scale < self.get_min_state():
+            self._min_state = (scale, pow_cons)
+            self[scale] = pow_cons
+        else:
+            LOG(msg='Current min power state is already lower.', log=Logs.WARN)
+
+class ContinuousStatePowerConsumption(AbstractPowerConsumption):
+    def __init__(self, min_scale, min_pow_cons, max_scale, max_pow_cons):
