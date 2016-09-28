8334472dcdc3264c31004995994dfa88c53a7589
Güner Orhan <gunerorhan2010@gmail.com>
2016-09-28 11:23:14 +0200

    New file path

--- LFOS/Resource/Type.py
+++ LFOS/Resource/Type.py
@@ -23,5 +23,11 @@ class Type:
     def same_abstraction(self, abs):
         return abs == self.abstraction
 
+    def get_abstraction(self):
+        return self.abstraction.split('.')[-1]
+
+    def get_identifier(self):
+        return self.identifier
+
     def __eq__(self, other):
         return self.same_abstraction(other.abstraction) and self.same_identifier(other.identifier) if isinstance(other, Type) else False
