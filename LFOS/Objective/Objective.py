b5b5b1292e68ba74f4351dba2a7afc57a08087f9
Güner Orhan <gunerorhan2010@gmail.com>
2016-10-26 12:05:52 +0200

    Changes to be committed: 	modified:   Objective/Objective.py * Constraint type is added to the ParameterTypes. The given objective is directly converted to the constraint formula and given solver with specified purpose.

--- LFOS/Objective/Objective.py
+++ LFOS/Objective/Objective.py
@@ -6,13 +6,14 @@ class ObjectivePurposes:
     MAXIMIZE = 'ObjectivePurposes.MAX'
 
 class ParameterTypes:
-    FUNCTION = 'ParameterTypes.FUNC'
-    CONSTANT = 'ParameterTypes.CONS'
+    FUNCTION = 'ParameterTypes.FUNCTION'
+    CONSTANT = 'ParameterTypes.CONSTANT'
+    FORMULA = 'ParameterTypes.CONSTRAINT'
 
 
 class Parameter:
     def __new__(cls, **kwargs):
-        if kwargs['type'] != ParameterTypes.CONSTANT and kwargs['type'] != ParameterTypes.FUNCTION:
+        if kwargs['type'] != ParameterTypes.CONSTANT and kwargs['type'] != ParameterTypes.FUNCTION and kwargs['type'] != ParameterTypes.FORMULA:
             LOG(msg='Given parameter type is not valid.', log=Logs.ERROR)
             return None
 
@@ -29,8 +30,10 @@ class Parameter:
     def eval(self):
         if self.__type == ParameterTypes.FUNCTION:
             return self.__rhs(**self.__kwargs)
-        else:
+        elif self.__type == ParameterTypes.CONSTANT:
             return self.__rhs
+        else:
+            return 0
 
     def get_type(self):
         return self.__type
@@ -58,12 +61,19 @@ class ObjectiveInterface(dict):
         return self.__purpose
 
     def add_parameter(self, **kwargs):
+        if 'parameter' in kwargs:
+            dict.__setitem__(self, kwargs['name'], kwargs['parameter'])
+            return kwargs['parameter']
+
         parameter = Parameter(**kwargs)
         if parameter:
-            self[kwargs['name']] = parameter
+            dict.__setitem__(self, kwargs['name'], parameter)
             return True
 
         return parameter
+
+    def __setitem__(self, key, value):
+        return self.add_parameter(name=key, parameter=value)
 
     def evaluate_parameters(self):
         return sum(value.eval() for param_name, value in self.items())
@@ -72,4 +82,4 @@ class ObjectiveInterface(dict):
         return self[param_name].eval()
 
     def __repr__(self):
-        return '%s --> %s' % (self.__purpose.split('.')[-1], ' + '.join([name if param.get_type() == ParameterTypes.CONSTANT else '%s(kwargs=%r)' % (name, param.get_kwargs()) for name, param in self.items()]))
+        return '%s --> %s' % (self.__purpose.split('.')[-1], ' + '.join(['%s(kwargs=%r)' % (name, param.get_kwargs()) if param.get_type() == ParameterTypes.FUNCTION else name for name, param in self.items()]))
